35d9d20ce345cef613664ba4a582c51a7a179172
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-10-07 22:57:09 +0200

    Ajout du bouton pour aller à l'université en voiture

--- app/src/main/java/com/example/onTime/MainActivity.java
+++ app/src/main/java/com/example/onTime/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.onTime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onUniversiteVoiturePressed(View view) {
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Université+d'Orléans,+Orléans,+France");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
