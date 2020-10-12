1ca533945509956701f1d16232757de427d54afe
ionasNeonakis <ionas.neonakis@gmail.com>
2020-10-12 17:45:33 +0200

    Fonction pour récupérer la date à partir d'une heure d'arrivée, à tester

--- app/src/main/java/com/example/onTime/MainActivity.java
+++ app/src/main/java/com/example/onTime/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.onTime.modele.Toolbox;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println(Toolbox.getDateFromHeureArrivee(39600));
     }
 }
--- app/src/main/java/com/example/onTime/modele/Toolbox.java
+++ app/src/main/java/com/example/onTime/modele/Toolbox.java
@@ -1,6 +1,7 @@
 package com.example.onTime.modele;
 
 
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -18,6 +19,32 @@ import java.util.GregorianCalendar;
 
     static public long getSecondesFromEpoch(Date date) {
         return (date.getTime() / 1000);
+    }
+
+    static public long getDateFromHeureArrivee(long arrivee) {
+        Calendar rightNow = Calendar.getInstance();
+        int hour = rightNow.get(Calendar.HOUR);
+        int minutes = rightNow.get(Calendar.MINUTE);
+        System.out.println("heure :" + hour);
+        System.out.println("minutes :" + minutes);
+        long secondsNow = Toolbox.getSecondesFromHeureMinute(hour, minutes);
+
+
+        Calendar gregorianCalendar = new GregorianCalendar(rightNow.get(Calendar.YEAR), rightNow.get(Calendar.MONTH), rightNow.get(Calendar.DAY_OF_MONTH), 0, 0);
+        Date dateajd = gregorianCalendar.getTime();
+        long dateEpoch = Toolbox.getSecondesFromEpoch(dateajd);
+
+        Date date = new Date();
+        System.out.println(date);
+
+
+        if (secondsNow < arrivee) {
+            System.out.println("ajd");
+            return dateEpoch + arrivee;
+        } else {
+            System.out.println("demain");
+            return dateEpoch + arrivee + 86400; // date ajourd'hui + heure de 11h + 24h en secondes pour demain
+        }
     }
 
 
