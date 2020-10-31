9fa640c4ed52ebf598203586ac22ab72dbb9c9cd
Ionas Neonakis <ionas.neonakis@gmail.com>
2020-10-31 10:20:23 +0100

    Implementation de l'interface parcelable afin de pouvoir les serealisizer de façon optimisée

--- app/src/main/java/com/example/onTime/modele/MRManager.java
+++ app/src/main/java/com/example/onTime/modele/MRManager.java
@@ -1,12 +1,15 @@
 package com.example.onTime.modele;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Classe représentant toutes les morning routines ainsi que l'heure d'arrivée du prochain trajet.
  */
-public class MRManager {
+public class MRManager implements Parcelable {
     private long heureArrivee;
     private List<MRA> listMRA;
 
@@ -19,9 +22,48 @@ public class MRManager {
     }
 
     public MRManager(long heureArrivee, List<MRA> listMRA) {
-        this.heureArrivee = heureArrivee; // 11h en secondes
+        this.heureArrivee = heureArrivee;
         this.listMRA = listMRA;
     }
+
+    public static final Parcelable.Creator<MRManager> CREATOR = new Parcelable.Creator<MRManager>() {
+        @Override
+        public MRManager createFromParcel(Parcel in) {
+            return new MRManager(in);
+        }
+
+        @Override
+        public MRManager[] newArray(int size) {
+            return new MRManager[size];
+        }
+    };
+
+    protected MRManager(Parcel in) {
+        heureArrivee = in.readLong();
+        if (in.readByte() == 0x01) {
+            listMRA = new ArrayList<>();
+            in.readList(listMRA, MRA.class.getClassLoader());
+        } else {
+            listMRA = null;
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(heureArrivee);
+        if (listMRA == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(listMRA);
+        }
+    }
+
 
     public long getHeureArrivee() {
         return this.heureArrivee;
@@ -35,15 +77,17 @@ public class MRManager {
         return listMRA;
     }
 
-    public boolean ajouterMRA(MRA mra){
+    public boolean ajouterMRA(MRA mra) {
         return this.listMRA.add(mra);
     }
 
-    public boolean removeMRA(MRA mra){
+    public boolean removeMRA(MRA mra) {
         return this.listMRA.remove(mra);
     }
 
-    public MRA removeMRA(int index){
+    public MRA removeMRA(int index) {
         return this.listMRA.remove(index);
     }
+
+
 }
