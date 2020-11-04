5bd58e0320bf43bb83b4f28c5192f3db8cdab819
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-11-04 23:22:52 +0100

    Le splash screen montre le logo de l'appli (petit pour l'instant) sur fond bleu

--- app/src/main/java/com/example/onTime/SplashScreenActivity.java
+++ app/src/main/java/com/example/onTime/SplashScreenActivity.java
@@ -13,6 +13,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
