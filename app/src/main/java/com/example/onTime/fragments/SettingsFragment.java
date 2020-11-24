b29096552ebbdc23c3b255bc7f0b5f3bdac2d906
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-11-24 22:28:33 +0100

    Premier jet pour le design + ajout code pour gérer le switch

--- app/src/main/java/com/example/onTime/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/onTime/fragments/SettingsFragment.java
@@ -1,60 +1,38 @@
 package com.example.onTime.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.onTime.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private SharedPreferences sharedPreferences;
+    private SwitchCompat switchNotificationsChaqueTache;
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
+    public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +40,31 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Récupération des sharedPreferences de l'application
+        Context context = this.getActivity().getApplicationContext();
+        this.sharedPreferences = context.getSharedPreferences("onTimePreferences", Context.MODE_PRIVATE);
+
+        // Récupération des éléments graphiques du fragment
+        this.switchNotificationsChaqueTache = view.findViewById(R.id.switchNotificationsDebutTaches);
+        Button boutonSupprimerDonnees = view.findViewById(R.id.boutonSupprimerDonnees);
+
+        // Mise à jour de l'état du switch en fonction de la valeur sauvegaardée dans les sharedPreferences
+        this.switchNotificationsChaqueTache.setChecked(this.sharedPreferences.getBoolean("notifyOnEachTaskStart", false));
+
+        // Association du listener qui va écouter les changements d'états du switch
+        switchNotificationsChaqueTache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharedPreferences.edit().putBoolean("notifyOnEachTaskStart", SettingsFragment.this.switchNotificationsChaqueTache.isChecked()).apply();
+            }
+        });
+
+        // TODO : On click listener du bouton pour afficher une pop up de confirmation avant de supprimer toutes les données utilisateur
     }
 }
