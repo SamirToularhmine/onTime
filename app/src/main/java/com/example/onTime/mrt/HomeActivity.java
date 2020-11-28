fe92361070fc6c78f390ed463485a60b394fef36
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2020-11-28 14:12:20 +0100

    Modification projet

--- app/src/main/java/com/example/onTime/mrt/HomeActivity.java
+++ app/src/main/java/com/example/onTime/mrt/HomeActivity.java
@@ -30,6 +30,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
