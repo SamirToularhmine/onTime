1967e70679886233ddbcafa647eb6574087adab5
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2020-11-28 14:00:24 +0100

    Introduction de l'application terminée ???

--- app/src/main/java/com/example/onTime/IntroActivity.java
+++ app/src/main/java/com/example/onTime/IntroActivity.java
@@ -1,10 +1,7 @@
 package com.example.onTime;
 
-import androidx.annotation.ColorRes;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
@@ -15,7 +12,6 @@ import android.os.Bundle;
 import com.example.onTime.mrt.HomeActivity;
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
-import com.github.paolorotolo.appintro.AppIntroViewPager;
 import com.github.paolorotolo.appintro.model.SliderPage;
 
 public class IntroActivity extends AppIntro {
@@ -31,24 +27,25 @@ public class IntroActivity extends AppIntro {
         this.UNSELECTED_PRIMARY_COLOR = Color.parseColor("#609dd1");
 
         SliderPage sp = new SliderPage();
-        sp.setTitle("Bienvenue sur onTime !");
-        sp.setDescription("Créer votre routine matinale n'a jamais été aussi simple !");
+        sp.setTitle(getString(R.string.slide0_title));
+        sp.setDescription(getString(R.string.slide0_desc));
         sp.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp.setBgColor(Color.WHITE);
         sp.setTitleColor(PRIMARY_COLOR);
         sp.setDescColor(PRIMARY_COLOR);
 
         SliderPage sp1 = new SliderPage();
-        sp1.setTitle("Routine matinale");
-        sp1.setDescription("Afin de ne plus jamais arriver en retard, créez votre routine matinale, ça se passe dans l'onglet Gérer > Mes morning routines");
+        sp1.setTitle(getString(R.string.slide1_title));
+        sp1.setDescription(getString(R.string.slide1_desc));
         sp1.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp1.setBgColor(Color.WHITE);
         sp1.setTitleColor(PRIMARY_COLOR);
         sp1.setDescColor(PRIMARY_COLOR);
+        sp1.setImageDrawable(R.drawable.slide1);
 
         SliderPage sp2 = new SliderPage();
-        sp2.setTitle("Tâches");
-        sp2.setDescription("Agrémentez votre routine matinale avec des tâches à réaliser pendant cette dernière. Un nom, une durée et c'est validé ! Vous pouvez même sauvegarder vos tâches pour les réutiliser dans d'autres Morning Routine : ce sont les tâches récurrentes !");
+        sp2.setTitle(getString(R.string.slide2_title));
+        sp2.setDescription(getString(R.string.slide2_desc));
         sp2.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp2.setBgColor(Color.WHITE);
         sp2.setTitleColor(PRIMARY_COLOR);
@@ -56,36 +53,40 @@ public class IntroActivity extends AppIntro {
         sp2.setImageDrawable(R.drawable.slide2);
 
         SliderPage sp3 = new SliderPage();
-        sp3.setTitle("Tâches récurrentes");
-        sp3.setDescription("Afin de ne pas toujours créer les même tâches, créez vos tâches récurrentes que vous pourrez réutiliser dans vos Morning Routine sans avoir à les recréer. Ça se passe dans l'onglet Gérer > Mes tâches récurrentes !");
+        sp3.setTitle(getString(R.string.slide3_title));
+        sp3.setDescription(getString(R.string.slide3_desc));
         sp3.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp3.setBgColor(Color.WHITE);
         sp3.setTitleColor(PRIMARY_COLOR);
         sp3.setDescColor(PRIMARY_COLOR);
+        sp3.setImageDrawable(R.drawable.slide3);
 
         SliderPage sp4 = new SliderPage();
-        sp4.setTitle("Trajets");
-        sp4.setDescription("Créez différents trajets pour vos Morning Routine. Ces trajets serviront de base de calcul pour l'application onTime afin de calculer votre heure de réveil à la minute près ! Pour les trajets, ça se passe dans l'onglet Gérer > Mes trajets !");
+        sp4.setTitle(getString(R.string.slide4_title));
+        sp4.setDescription(getString(R.string.slide4_desc));
         sp4.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp4.setBgColor(Color.WHITE);
         sp4.setTitleColor(PRIMARY_COLOR);
         sp4.setDescColor(PRIMARY_COLOR);
+        sp4.setImageDrawable(R.drawable.slide4);
 
         SliderPage sp5 = new SliderPage();
-        sp5.setTitle("Heure d'arrivée");
-        sp5.setDescription("Définissez une heure d'arrivée à votre point de destination pour le lendemain. Ainsi, vous pourrez cliquer sur la cloche pour mettre en place le prochain réveil à l'heure calculée !");
+        sp5.setTitle(getString(R.string.slide5_title));
+        sp5.setDescription(getString(R.string.slide5_desc));
         sp5.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp5.setBgColor(Color.WHITE);
         sp5.setTitleColor(PRIMARY_COLOR);
         sp5.setDescColor(PRIMARY_COLOR);
+        sp5.setImageDrawable(R.drawable.slide5);
 
         SliderPage sp6 = new SliderPage();
-        sp6.setTitle("Bonne nuit !");
-        sp6.setDescription("Ça y est ! Vous êtes près à aller vous coucher, l'application se charge du reste ! ;)");
+        sp6.setTitle(getString(R.string.slide6_title));
+        sp6.setDescription(getString(R.string.slide6_desc));
         sp6.setImageDrawable(R.drawable.ic_alarm_on_56px);
         sp6.setBgColor(Color.WHITE);
         sp6.setTitleColor(PRIMARY_COLOR);
         sp6.setDescColor(PRIMARY_COLOR);
+        sp6.setImageDrawable(R.drawable.slide6);
 
         this.setImmersive(true);
         this.setColorSkipButton(PRIMARY_COLOR);
